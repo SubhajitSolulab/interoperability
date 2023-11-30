e72948b94ff0a8d72d61b1725846ab1876743c2a
SubhajitSolulab <subhajit@solulab.co>
2023-11-30 18:54:15 +0530

    added modal and fixes ui

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -17,7 +17,14 @@ const Home = () => {
     message: "",
     document: "",
   });
-  console.log(import.meta.env.VITE_BACKEND_URL);
+  const [hashes, setHashes] = useState({
+    txMessageMumbaiHash: "",
+    txMessageBscHash: "",
+    txDocumentMumbaiHash: "",
+    txDocumentBscHash: "",
+  });
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  // console.log(import.meta.env.VITE_BACKEND_URL);
 
   const handleNameChange = (e) => setName(e.target.value);
   const handleMessageChange = (e) => setMessage(e.target.value);
@@ -37,13 +44,21 @@ const Home = () => {
     formData.append("name", name);
     formData.append("document", file);
     formData.append("message", message);
-    const res = await axios.post(
+    const response = await axios.post(
       `${import.meta.env.VITE_BACKEND_URL}/write/handleDataWrite`,
       formData
     );
+    setHashes({
+      txMessageMumbaiHash: response.data.data?.txMessage?.txMessageMumbaiHash,
+      txMessageBscHash: response.data.data?.txMessage?.txMessageBscHash,
+      txDocumentMumbaiHash:
+        response.data.data?.txDocument?.txDocumentMumbaiHash,
+      txDocumentBscHash: response.data.data?.txDocument?.txDocumentBscHash,
+    });
     setMyLoader(false);
 
-    console.log(res);
+    setIsModalOpen(true);
+    console.log(response);
   };
 
   const handleMumbaiQuery = async () => {
@@ -98,7 +113,7 @@ const Home = () => {
 
       <div className="flex items-center justify-center py-4">
         <div className="text-6xl font-bold mb-4 border-b-4 border-gray-700">
-          Mumbai↔BSC
+          Mumbai↔BSC Testnet
         </div>
       </div>
       <div className="flex flex-col items-center justify-center">
@@ -146,9 +161,9 @@ const Home = () => {
           </form>
         </div>
       </div>
-      <div className="flex justify-around my-10">
-        <div className="flex flex-col items-center mb-4">
-          <h2 className=" text-2xl mb-2">Read Data from Mumbai</h2>
+      <div className="flex justify-around items-stretch my-10">
+        <div className="flex flex-col items-center mb-4 w-1/2">
+          <h2 className="text-2xl mb-2">Read Data from Mumbai</h2>
           <input
             type="text"
             placeholder="name"
@@ -162,16 +177,16 @@ const Home = () => {
           >
             query
           </button>
-          <div className=" px-5 w-4/5">
-            <p className="text-gray-300 mt-4">Message : {mumbaiData.message}</p>
+          <div className="px-5 w-4/5">
+            <p className="text-gray-300 mt-4">Message: {mumbaiData.message}</p>
             <p className="text-gray-300 mt-4">
-              Document : {mumbaiData.document}
+              Document: {mumbaiData.document}
             </p>
           </div>
         </div>
-        <div className="border-r-2 border-gray-400"></div>
-        <div className="flex flex-col items-center">
-          <h2 className=" text-2xl mb-2">Read Data from BSC</h2>
+        <div className="border-r-2 border-gray-400 self-stretch mx-6"></div>
+        <div className="flex flex-col items-center mb-4 w-1/2">
+          <h2 className="text-2xl mb-2">Read Data from BSC Testnet</h2>
           <input
             type="text"
             placeholder="name"
@@ -189,6 +204,95 @@ const Home = () => {
             <p className="text-gray-300 mt-4">Message: {bscData.message}</p>
             <p className="text-gray-300 mt-4">Document: {bscData.document}</p>
           </div>
+        </div>
+      </div>
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        data={hashes}
+      />
+    </div>
+  );
+};
+
+/* eslint-disable react/prop-types */
+const Modal = ({ isOpen, onClose, data }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+      <div className="bg-gray-800 p-5 rounded-lg">
+        {data?.txMessageMumbaiHash && (
+          <p className="text-white text-lg mb-4">
+            polygonMumbaiMessage:{" "}
+            <a
+              href={
+                "https://mumbai.polygonscan.com/tx/" + data?.txMessageMumbaiHash
+              }
+              rel="noreferrer"
+              target="_blank"
+            >
+              <span className=" border-b-2 border-blue-800 text-blue-600 pl-2">
+                {data?.txMessageMumbaiHash}
+              </span>
+            </a>
+          </p>
+        )}
+
+        {data?.txMessageBscHash && (
+          <p className="text-white text-lg mb-4">
+            polygonMumbaiMessage:{" "}
+            <a
+              href={"https://testnet.bscscan.com/tx/" + data?.txMessageBscHash}
+              rel="noreferrer"
+              target="_blank"
+            >
+              <span className=" border-b-2 border-blue-800 text-blue-600 pl-2">
+                {data?.txMessageBscHash}
+              </span>
+            </a>
+          </p>
+        )}
+
+        {data?.txDocumentMumbaiHash && (
+          <p className="text-white text-lg mb-4">
+            polygonMumbaiMessage:{" "}
+            <a
+              href={
+                "https://mumbai.polygonscan.com/tx/" +
+                data?.txDocumentMumbaiHash
+              }
+              rel="noreferrer"
+              target="_blank"
+            >
+              <span className=" border-b-2 border-blue-800 text-blue-600 pl-2">
+                {data?.txDocumentMumbaiHash}
+              </span>
+            </a>
+          </p>
+        )}
+
+        {data?.txDocumentBscHash && (
+          <p className="text-white text-lg mb-4">
+            polygonMumbaiMessage:{" "}
+            <a
+              href={"https://testnet.bscscan.com/tx/" + data?.txDocumentBscHash}
+              rel="noreferrer"
+              target="_blank"
+            >
+              <span className=" border-b-2 border-blue-800 text-blue-600 pl-2">
+                {data?.txDocumentBscHash}
+              </span>
+            </a>
+          </p>
+        )}
+        <div className="text-center">
+          <button
+            onClick={onClose}
+            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 focus:outline-none"
+          >
+            Close
+          </button>
         </div>
       </div>
     </div>
